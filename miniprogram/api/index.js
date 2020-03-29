07c83779310df219a6b6f0633927bfa119cc2681
SanLangGuo <SanLangGuo>
2020-03-29 11:44:10 +0800

    add user select

--- miniprogram/api/index.js
+++ miniprogram/api/index.js
@@ -1,10 +1,13 @@
-// const db = wx.cloud.database();
+const db = wx.cloud.database();
 const API = {
   async login() {
     return await wx.cloud.callFunction({name: 'login'});
   },
-  async inputUserInfo() {
-    return await wx.cloud.callFunction({name: 'login'});
+  async getUserInfo(_openid) {
+    return await db.collection('user').where({_openid}).get();
+  },
+  async insetUserInfo(data) {
+    return await db.collection('user').add({data});
   }
 };
 
--- miniprogram/pages/my/index.js
+++ miniprogram/pages/my/index.js
@@ -5,20 +5,42 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userInfo: '',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   async onLoad(options) {
-    const res = await API.login();
-    console.log(res,'-login')
-    wx.getUserInfo({
-      success(res) {
-        console.log(res)
+    const userInfo = wx.getStorageSync('userInfo');
+    if (userInfo) {
+      this.setData({
+        userInfo
+      })
+    } else {
+      const res = await API.login();
+      const {
+        openid
+      } = res.result;
+      const userInfo = await API.getUserInfo(openid);
+      if (userInfo.data && userInfo.data.length) {
+        wx.setStorageSync('userInfo', userInfo.data[0]);
+        this.setData({
+          userInfo: userInfo.data[0]
+        })
+      } else {
+        console.log(1)
+        wx.getUserInfo({
+          success (res) {
+            console.log(1)
+            const data = Object.assign({}, res.userInfo);
+            data._openid = openid;
+            // const result = await API.insetUserInfo(data)
+            console.log(result)
+          }
+        })
       }
-    })
+    }
   },
 
   /**
