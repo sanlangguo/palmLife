23dc5eb31fa9fd12723a82303045fb2fdd119e9e
runman <781611293@qq.com>
2020-04-07 15:14:27 +0800

    change

--- application/miniprogram/api/index.js
+++ application/miniprogram/api/index.js
@@ -11,7 +11,7 @@ const API = {
   },
   async updateUserInfo(openid, data) {
     console.log(openid, data, '---')
-    return await db.collection('user').doc('dc65fe3e5e80a04b001b22f075115686').update({data: { receiveCity: "河南省郑州市新郑市"}});
+    return await db.collection('user').doc('dc65fe3e5e80a04b001b22f075115686').update({data: { receiveCity: "河南省郑州市新郑市1"}});
   }
 };
 
